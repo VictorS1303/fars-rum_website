8fc4fae26ce8ee8d6faf00ba975994f2891ecf6d
Victor Schweitz <victorschweitz@hotmail.com>
2024-05-30 07:36:45 +0200

    Nothing

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,7 +1,9 @@
 export const data = await fetch("https://qftguxwmsqzradjlguhs.supabase.co/rest/v1/artist_cards", {
     method: "GET",
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdGd1eHdtc3F6cmFkamxndWhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1NDIwOTUsImV4cCI6MjAzMTExODA5NX0.uglTx3133whZEgoLakBYoMK3mGTNGEB5OWvUO1H1AWY",
+        apikey: "xxx",
     }
 }).then(res => res.json())
 .catch((error) => console.log(error))
+
+
